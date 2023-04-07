f0ec1414570a3d88837e66ddabee89898a665fdc
Guillen Julian <guillen.julian1985@gmail.com>
2023-04-07 19:01:59 -0300

    test deploy 7

--- back/db.ts
+++ back/db.ts
@@ -4,9 +4,21 @@ import * as serviceAccount from "../key.json";
 const varEnv: any = process.env;
 
 console.log(varEnv.DB_URl, "ESTA ES LA VARIABLE DE ENTORNO");
+const firebaseConfig: any = {
+  type: varEnv.TIPE,
+  project_id: varEnv.PROJECT_ID,
+  private_key_id: varEnv.PRIVATE_KEY_ID,
+  private_key: varEnv.PRIVATE_KEY,
+  client_email: varEnv.CLIENT_EMAIL,
+  client_id: varEnv.CLIENT_ID,
+  auth_uri: varEnv.AUTH_URI,
+  token_uri: varEnv.TOKEN_URI,
+  auth_provider_X509_cert_url: varEnv.AUTH_PROVIDER_X509_CERT_URL,
+  client_X509_cert_url: varEnv.CLIENT_X509_CERT_URL,
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
+  credential: admin.credential.cert(firebaseConfig),
   databaseURL: varEnv.DB_URl,
 });
 
