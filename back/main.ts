01e46497b62fb6d007ec64802ed36f01c26991c9
Guillen Julian <guillen.julian1985@gmail.com>
2023-04-07 20:25:46 -0300

    test deploy 10

--- back/main.ts
+++ back/main.ts
@@ -3,7 +3,7 @@ import { db, rtdb } from "./db";
 import * as cors from "cors";
 import { uuidv4 } from "@firebase/util";
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
